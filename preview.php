1c7cb2258bcda1748d3a1da97da69d71e240597b
Mark Blashki <markblashki1@gmail.com>
2022-08-02 10:25:28 +1000

    add: allow seek on preview player

--- preview.php
+++ preview.php
@@ -129,13 +129,15 @@ class MediaPreview extends OBFController
     if($type=='audio') 
     {
 
+      // Get file size
+      $filesize = filesize($media_file);
+      
       // download mode
       if($download)
       {
         header('Content-Description: File Transfer');
         header('Content-Disposition: attachment; filename='.$download_filename);
         header('Content-Type: application/octet-stream');
-        header('Content-Length: '.filesize($media_file));
 
         $fp = fopen($media_file,'rb');
         fpassthru($fp);
@@ -154,17 +156,86 @@ class MediaPreview extends OBFController
         if($audio_format == 'mp3') exec(strtr(OB_TRANSCODE_AUDIO_MP3,$strtr_array));
         else exec(strtr(OB_TRANSCODE_AUDIO_OGG,$strtr_array));
       }
+      
+      // Source: https://stackoverflow.com/questions/11340276/make-mp3-seekable-php
+      // Handle 'Range' header
+      if(isset($_SERVER['HTTP_RANGE'])){
+        $range = $_SERVER['HTTP_RANGE'];
+      } elseif($apache = apache_request_headers()){
+        $headers = array();
+        foreach ($apache as $header => $val){
+          $headers[strtolower($header)] = $val;
+        }
+        if(isset($headers['range'])){
+          $range = $headers['range'];
+        }
+        else $range = FALSE;
+      }
+      else $range = FALSE;
 
+      //Is range
+      if($range){
+        $partial = true;
+        list($param, $range) = explode('=',$range);
+        // Bad request - range unit is not 'bytes'
+        if(strtolower(trim($param)) != 'bytes'){ 
+          header("HTTP/1.1 400 Invalid Request");
+          exit;
+        }
+        // Get range values
+        $range = explode(',',$range);
+        $range = explode('-',$range[0]); 
+        // Deal with range values
+        if ($range[0] === ''){
+          $end = $filesize - 1;
+          $start = $end - intval($range[0]);
+        } else if ($range[1] === '') {
+          $start = intval($range[0]);
+          $end = $filesize - 1;
+        }else{ 
+          // Both numbers present, return specific range
+          $start = intval($range[0]);
+          $end = intval($range[1]);
+          if ($end >= $filesize || (!$start && (!$end || $end == ($filesize - 1)))) $partial = false; // Invalid range/whole file specified, return whole file
+        }
+        $length = $end - $start + 1;
+      }
+      // No range requested
+      else $partial = false; 
+      
+      header('Accept-Ranges: bytes');
+      header("Content-Length: " . ($partial ? $length : $filesize));
+      
       // temporary 
       header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
       header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 
       if($audio_format == 'mp3') header('Content-Type: audio/mpeg');
       else header("Content-Type: audio/ogg");
-      header('Content-Length: '.filesize($cache_file));
-      $fp = fopen($cache_file,'rb');
-      fpassthru($fp);
-
+      
+      // send extra headers for range handling...
+      if ($partial) {
+        header('HTTP/1.1 206 Partial Content');
+        header("Content-Range: bytes $start-$end/$filesize");
+        if (!$fp = fopen($cache_file, 'rb')) {
+            header("HTTP/1.1 500 Internal Server Error");
+            exit;
+        }
+        if ($start) fseek($fp,$start);
+        while($length){
+            set_time_limit(0);
+            $read = ($length > 8192) ? 8192 : $length;
+            $length -= $read;
+            print(fread($fp,$read));
+        }
+        fclose($fp);
+      }
+      //just send the whole file
+      else {
+        header('Content-Length: '.filesize($cache_file));
+        $fp = fopen($cache_file,'rb');
+        fpassthru($fp);
+      }
     }
 
     elseif($type=='video')
